ef2a0249998ab7561a4b34af1dab665ec220ef90
SoGoesLight <ilyalpt@gmail.com>
2020-09-24 18:57:16 +0300

    Initial commit

---
+++ src/main/scala/basics/Basics.scala
@@ -0,0 +1,18 @@
+package basics
+
+import scala.annotation.tailrec
+
+object Basics {
+
+  // Least common multiple
+  def lcm(a: Int, b: Int): Int =
+    if (a == 0 && b == 0) 0
+    else Math.abs(a * b) / gcd(a, b)
+
+  // Greatest common divisor
+  @tailrec
+  def gcd(a: Int, b: Int): Int =
+    if (b == 0) Math.abs(a)
+    else gcd(b, a % b)
+
+}
---
+++ src/test/scala/basics/BasicsSpec.scala
@@ -0,0 +1,21 @@
+package basics
+
+import basics.Basics._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+
+class BasicsSpec extends AnyFlatSpec {
+  "gcd" should "be correct" in {
+    gcd(0, 0) shouldEqual 0
+    gcd(2, 6) shouldEqual 2
+    gcd(-2, 6) shouldEqual 2
+    gcd(653356, 356) shouldEqual 4
+  }
+
+  "lcm" should "be correct" in {
+    lcm(45, 4) shouldEqual 180
+    lcm(7, 15) shouldEqual 105
+    lcm(653356, 356) shouldEqual 58148684
+  }
+
+}
